c0e13497792fb24a3ddfd31eedaafdd435ad737c
Nuno Ribeiro <nunomonizribeiro@gmail.com>
2024-01-13 13:45:34 +0000

    Fix pick_up animation not playing

--- Objects/Scripts/Transporter/StateMachine/PickUpStone.gd
+++ Objects/Scripts/Transporter/StateMachine/PickUpStone.gd
@@ -3,9 +3,11 @@ class_name PickUpStone
 
 @onready var animation = $"../../AnimatedSprite2D"
 @onready var transporter = $"../.."
+var picked_up = false
 
 func enter():
 	print("Pick Up Stone")
+	picked_up = false
 	if transporter.quarry.stone_pile.size() > 0:
 		animation.play("Pick_Up")
 	transporter.moving = false
@@ -15,14 +17,20 @@ func update(delta):
 			print("Im out")
 			if transporter.stone_transported == 0:
 				Transitioned.emit(self, "Wait")
-			Transitioned.emit(self, "DeliverStone")
+			elif picked_up:
+				Transitioned.emit(self, "DeliverStone")
 			
 	if transporter.transport_quantity.total_value > transporter.stone_transported:
 		var stone_grabbed = transporter.quarry.stone_pile.pop_back()
 		if(stone_grabbed != null):
 			stone_grabbed.queue_free()
 			transporter.stone_transported += 1
-			print("Transporting: ", transporter.stone_transported)
 	else:
-		Transitioned.emit(self, "DeliverStone")
+		if picked_up: 
+			Transitioned.emit(self, "DeliverStone")
 	
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if animation.animation == "Pick_Up":
+		picked_up = true
