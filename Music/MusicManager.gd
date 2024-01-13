bd3477460160b93a71187eb5734210630c8c4235
Nuno Ribeiro <nunomonizribeiro@gmail.com>
2024-01-13 14:06:33 +0000

    Add template for music manager

---
+++ Music/MusicManager.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var fase1 : AudioStreamWAV
+@export var fase2 : AudioStreamWAV
+@export var fase3 : AudioStreamWAV
+@export var fase4 : AudioStreamWAV
+@export var fase5 : AudioStreamWAV
+@export var fase6 : AudioStreamWAV
+@export var fase7 : AudioStreamWAV
+@export var fase8 : AudioStreamWAV
+
+func _ready():
+	#play fase 1
+	pass
+
+func _process(delta):
+	#calculos para decidir mudança da música
