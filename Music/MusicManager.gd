025b63ac3fed6d5fc58ba5fbb971d0a94ecaa6d2
Nuno Ribeiro <nunomonizribeiro@gmail.com>
2024-01-13 14:07:49 +0000

    Add template for music manager FIX

--- Music/MusicManager.gd
+++ Music/MusicManager.gd
@@ -8,10 +8,13 @@ extends Node2D
 @export var fase6 : AudioStreamWAV
 @export var fase7 : AudioStreamWAV
 @export var fase8 : AudioStreamWAV
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 func _ready():
 	#play fase 1
+	audio_stream_player_2d.play(fase1)
 	pass
 
 func _process(delta):
 	#calculos para decidir mudança da música
+	pass
