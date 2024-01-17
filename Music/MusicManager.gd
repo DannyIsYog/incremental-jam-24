c7b53d47bfc9f7834fcb4392164bac6c0c85cae3
turradaturra <sotcpc12@gmail.com>
2024-01-17 20:00:24 +0000

    Add music manager

--- Music/MusicManager.gd
+++ Music/MusicManager.gd
@@ -20,10 +20,25 @@ func _ready():
 
 func _process(delta):
 	
-	if andar <= 8:
+	if andar <= 12:
 		audio_stream_player_2d.stream = fase1
-	if  12 <= andar  && andar <= 20:
+		audio_stream_player_2d.play() #talvez tenha colocar a linha 18 aqui
+	if 12 < andar && andar <= 24:
 		audio_stream_player_2d.stream = fase2
+	if 24 < andar && andar <= 36:
+		audio_stream_player_2d.stream = fase3
+	if 36 < andar && andar <= 48:
+		audio_stream_player_2d.stream = fase4
+	if 48 < andar && andar <= 60:
+		audio_stream_player_2d.stream = fase5
+	if 60 < andar && andar <= 72:
+		audio_stream_player_2d.stream = fase6
+	if 72 < andar && andar <= 85:
+		audio_stream_player_2d.stream = fase7
+	if 85 < andar && andar <= 100:
+		audio_stream_player_2d.stream = fase8
+		
+	pass
 
 func change_song_to(song):
 	audio_stream_player_2d.stream = song
